d4c0d0209b02698e38f5847061d5ebfb25b411a7
Hichem <hichem.dimassi@liberrex.com>
2024-09-16 13:53:42 +0100

    added typewriter effect package

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,35 +1,78 @@
-import React, { useState } from 'react';
-import { Section, SectionText, SectionTitle, SectionTitleSecondary } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import Button2 from '../../styles/GlobalComponents/Button2';
-import { LeftSection, RightSection } from './HeroStyles';
-import { Img } from '../Projects/ProjectsStyles';
-import { AiFillCopy } from 'react-icons/ai';
-import Typical from 'react-typical'
-
+import React, { useState } from "react";
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+  SectionTitleSecondary,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import Button2 from "../../styles/GlobalComponents/Button2";
+import { LeftSection, RightSection } from "./HeroStyles";
+import { Img } from "../Projects/ProjectsStyles";
+import { AiFillCopy } from "react-icons/ai";
+import Typical from "react-typical";
+import Typewriter from "typewriter-effect";
 
 const Hero = (props) => {
-  const [email, setEmail] = useState("hichem.dimassi@esprit.tn");
+  const [email, setEmail] = useState("hichem.dimassi@gmail.com");
   const [action, setaction] = useState("Download CV");
   return (
     <Section row nopadding>
       <LeftSection>
         <SectionTitle main center>
-          Hello, I am hichem.
+          <Typewriter
+            options={{
+              delay: 40, // Typing delay (in milliseconds)
+              autoStart: true,
+            }}
+            onInit={(typewriter) => {
+              typewriter.typeString("Hello, I am hichem.").start();
+            }}
+          />
         </SectionTitle>
         <SectionTitleSecondary>
-          I'm a Web/mobile developper.
+        <Typewriter
+            options={{
+              delay: 50, // Typing delay (in milliseconds)
+              autoStart: true,
+            }}
+            onInit={(typewriter) => {
+              typewriter.pauseFor(1500)
+              typewriter.typeString('A Passionate Software Engineer.').start();
+            }}
+          />
         </SectionTitleSecondary>
-        <Button onClick={() => { setEmail("Email Copied!"); setTimeout(() => { setEmail("hichem.dimassi@esprit.tn"); }, 1500); navigator.clipboard.writeText('hichem.dimassi@esprit.tn') }}>{email}<AiFillCopy size="3rem" style={{ marginLeft: "10px" }} /></Button>
+        <Button
+          onClick={() => {
+            setEmail("Email Copied!");
+            setTimeout(() => {
+              setEmail("hichem.dimassi@gmail.com");
+            }, 1500);
+            navigator.clipboard.writeText("hichem.dimassi@gmail.com");
+          }}
+        >
+          {email}
+          <AiFillCopy size="3rem" style={{ marginLeft: "10px" }} />
+        </Button>
         <a href="/images/CV_Hichem_Dimassi.pdf" download>
-          <Button2 onClick={() => { setaction("Downloading..."); setTimeout(() => { setaction("Download CV"); }, 2500); }}>{action}<AiFillCopy size="3rem" style={{ marginLeft: "10px" }} /></Button2>
+          <Button2
+            onClick={() => {
+              setaction("Downloading...");
+              setTimeout(() => {
+                setaction("Download CV");
+              }, 2500);
+            }}
+          >
+            {action}
+            <AiFillCopy size="3rem" style={{ marginLeft: "10px" }} />
+          </Button2>
         </a>
       </LeftSection>
       <RightSection>
         <Img src="/images/macFinal.png" />
       </RightSection>
     </Section>
-  )
+  );
 };
 
 export default Hero;
